9385e8c919c0477c80d817e429b164d34d2ac457
PARWEEN Fazrana <D4J@dsone.3ds.com>
2024-12-14 12:06:16 +0530

    BranchB

--- src/practice/PracticeTest1.java
+++ src/practice/PracticeTest1.java
@@ -4,6 +4,7 @@ public class PracticeTest1 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class1");
 		System.out.println("Hello_class1 new changes");
-		System.out.println("Hello_class1 Branch B");
+		System.out.println("Hello_class1 Branch B,branch B");
+		System.out.println("Hello_class1 Branch B,New");
 	}
 }
