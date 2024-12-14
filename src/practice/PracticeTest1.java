86dd9c005c4fb9f661b8fdf50dd25d8d2e4b1dcf
PARWEEN Fazrana <D4J@dsone.3ds.com>
2024-12-14 11:30:46 +0530

    FazA

--- src/practice/PracticeTest1.java
+++ src/practice/PracticeTest1.java
@@ -3,5 +3,6 @@ package practice;
 public class PracticeTest1 {
 	public static void main(String[] args) {
 		System.out.println("Hello_class1");
+		System.out.println("Hello_class1 new changes");
 	}
 }
